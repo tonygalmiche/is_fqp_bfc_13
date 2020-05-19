944495af8c199dbc7708ce84cbfb7d120e45648f
Tony Galmiche <tony.galmiche@infosaone.com>
2020-05-19 09:17:36 +0200

    Mise en ligne

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Module odoo 13 pour FQP-BFC",
+    "summary": "Module odoo 13 pour FQP-BFC",
+    "version": "13.0.1.0.0",
+    "author": "Tony Galmiche / InfoSaône",
+    "website": "https://infosaone.com",
+    "category": "InfoSaône",
+    "depends": [
+        "event",
+        "mass_mailing",
+        "project",
+        "survey",
+        "website",
+        "website_blog",
+        "website_event"
+    ],
+    "data": [
+        "views/event_templates.xml",
+        "views/event_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+}
---
+++ models/event.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import _, api, fields, models
+
+
+class EventRegistration(models.Model):
+    _inherit = 'event.registration'
+
+    is_societe = fields.Char(string='Société')
+
