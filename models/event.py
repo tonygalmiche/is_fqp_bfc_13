a5021ae9d4893892acb15641eff87c3c55fd7888
Tony Galmiche <tony.galmiche@infosaone.com>
2020-05-28 08:59:48 +0200

    add questions aux participants

--- models/event.py
+++ models/event.py
@@ -6,5 +6,7 @@ from odoo import _, api, fields, models
 class EventRegistration(models.Model):
     _inherit = 'event.registration'
 
-    is_societe = fields.Char(string='Société')
-
+    is_societe     = fields.Char(string='Société')
+    is_fonction    = fields.Char(string='Fonction')
+    is_adherent    = fields.Selection([('Oui', 'Oui'),('Non', 'Non')], string='Adhérent')
+    is_commentaire = fields.Char(string='Commentaire')
