65c85cbcf88137c8407ed5f62403c161588cafb0
Tony Galmiche <tony.galmiche@infosaone.com>
2021-09-29 10:26:13 +0200

    modif

--- models/event.py
+++ models/event.py
@@ -9,4 +9,4 @@ class EventRegistration(models.Model):
     is_societe     = fields.Char(string='Société')
     is_fonction    = fields.Char(string='Fonction')
     is_adherent    = fields.Selection([('Oui', 'Oui'),('Non', 'Non')], string='Adhérent')
-    is_commentaire = fields.Char(string='Commentaire')
+    is_commentaire = fields.Char(string='Avez-vous des besoins spécifiques?')
